9012cdcc447b23ac92164d312cd77d85b5124dcc
joyeuxmathieu <joyeux.mathieu06@gmail.com>
2026-03-13 10:04:05 +0100

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCw2c98GzmqT0snVaKTrkm2Kcz4ndlBK7M",
+  authDomain: "alphark-f1982.firebaseapp.com",
+  projectId: "alphark-f1982",
+  storageBucket: "alphark-f1982.firebasestorage.app",
+  messagingSenderId: "120277911559",
+  appId: "1:120277911559:web:20a6823b44eda2e07082a2"
+};
+
+firebase.initializeApp(firebaseConfig);
