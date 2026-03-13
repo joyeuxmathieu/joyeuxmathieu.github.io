580d286b05773755f0e3d36323b2872f380365c1
joyeuxmathieu <joyeux.mathieu06@gmail.com>
2026-03-13 14:34:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, OAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-
 const firebaseConfig = {
-
 apiKey: "AIzaSyCW2c98GzmqT0snVaKTRkm2KcZ4ndlBK7M",
 authDomain: "alphark-f1982.firebaseapp.com",
 projectId: "alphark-f1982"
-
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
+const btn = document.getElementById("discord-btn");
+const userInfo = document.getElementById("user-info");
+
+// ===============================
+// UTILISATEUR SAUVEGARDE
+// ===============================
+
+const savedUser = localStorage.getItem("discord_user");
+
+if(savedUser){
+const data = JSON.parse(savedUser);
+showUser(data.username,data.avatar);
+}
 
 // ===============================
 // CONNEXION DISCORD
 // ===============================
 
-window.loginDiscord = function(){
+btn.addEventListener("click", loginDiscord);
+
+function loginDiscord(){
 
 const provider = new OAuthProvider('oidc.discord');
 
@@ -27,22 +39,23 @@ signInWithPopup(auth, provider)
 .then((result)=>{
 
 const credential = OAuthProvider.credentialFromResult(result);
-
 const idToken = credential.idToken;
 
 const payload = JSON.parse(atob(idToken.split('.')[1]));
 
 const username = payload.username;
+const avatar = payload.avatar;
+const id = payload.sub;
 
+const avatarURL =
+"https://cdn.discordapp.com/avatars/"+id+"/"+avatar+".png";
 
 localStorage.setItem("discord_user", JSON.stringify({
-username: username
+username: username,
+avatar: avatarURL
 }));
 
-document.getElementById("discord-btn").style.display = "none";
-
-document.getElementById("user-info").innerHTML =
-"🟢 " + username + ' <span onclick="logout()" style="cursor:pointer;color:red;">⏻</span>';
+showUser(username,avatarURL);
 
 })
 
@@ -52,37 +65,15 @@ console.log(error);
 
 }
 
-
 // ===============================
-// DECONNEXION
+// AFFICHER UTILISATEUR
 // ===============================
 
-window.logout = function(){
+function showUser(username,avatar){
 
-localStorage.removeItem("discord_user");
+btn.style.display="none";
 
-location.reload();
+userInfo.innerHTML =
+'<img src="'+avatar+'" class="avatar"> 🟢 '+username;
 
 }
-
-
-// ===============================
-// VERIFICATION UTILISATEUR
-// ===============================
-
-document.addEventListener("DOMContentLoaded", function(){
-
-const user = localStorage.getItem("discord_user");
-
-if(user){
-
-const data = JSON.parse(user);
-
-document.getElementById("discord-btn").style.display = "none";
-
-document.getElementById("user-info").innerHTML =
-"🟢 " + data.username + ' <span onclick="logout()" style="cursor:pointer;color:red;">⏻</span>';
-
-}
-
-});
